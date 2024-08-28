c6d901bc15dca014ab39bc1994351f66fd5d6384
Gokulkrishnan <130207834+gokul-webdev@users.noreply.github.com>
2024-08-28 20:12:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right' });
 /*============ typed js ===========*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['App Developer' , 'MERN Stack' ],
+    strings: ['Full Stack Web Developer' , 'Front End Developer' ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
